e2a19d08aeae012cc938f51527a703a6d6996629
Paul Theriault <ptheriault@mozilla.com>
2019-06-27 20:01:11 +1000

    added Firefox 67.0.2

---
+++ browser/omni/chrome/browser/content/browser/syncedtabs/sidebar.js
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const {SyncedTabs} = ChromeUtils.import("resource://services-sync/SyncedTabs.jsm");
+const {SyncedTabsDeckComponent} = ChromeUtils.import("resource:///modules/syncedtabs/SyncedTabsDeckComponent.js");
+const {LightweightThemeChild} = ChromeUtils.import("resource:///actors/LightweightThemeChild.jsm");
+
+ChromeUtils.defineModuleGetter(this, "fxAccounts",
+                               "resource://gre/modules/FxAccounts.jsm");
+
+var syncedTabsDeckComponent = new SyncedTabsDeckComponent({window, SyncedTabs, fxAccounts});
+
+let themeListener;
+
+let onLoaded = () => {
+  themeListener = new LightweightThemeChild({
+    content: window,
+    chromeOuterWindowID: window.top.windowUtils.outerWindowID,
+    docShell: window.docShell,
+  });
+  syncedTabsDeckComponent.init();
+  document.getElementById("template-container").appendChild(syncedTabsDeckComponent.container);
+};
+
+let onUnloaded = () => {
+  if (themeListener) {
+    themeListener.cleanup();
+  }
+  removeEventListener("DOMContentLoaded", onLoaded);
+  removeEventListener("unload", onUnloaded);
+  syncedTabsDeckComponent.uninit();
+};
+
+addEventListener("DOMContentLoaded", onLoaded);
+addEventListener("unload", onUnloaded);
